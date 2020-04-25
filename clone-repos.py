ebfccb813b46a7596e062e062f01525465563a58
madigan-99 <fymadigan@outlook.com>
2020-04-25 13:32:59 -0700

    variable names

--- clone-repos.py
+++ clone-repos.py
@@ -1,12 +1,11 @@
 import os 
 
 stuName = [];
-
 beginning = "https://github.com/info-201a-sp20/"
 url = input("What's the assignment name? ")
 url = url + "-"
 
 for student in stuName:
 	temp = (beginning + url + student + ".git")
-	__temp__ = f'{temp}'
-	os.system("git clone {}".format(__temp__))
+	repo_url = f'{temp}'
+	os.system("git clone {}".format(repo_url))
